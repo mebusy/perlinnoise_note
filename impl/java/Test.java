161e620b65482a6ba82679f6e23aae6506a6492a
Qi Binyi <qibinyi@Qis-mini.local>
2021-10-06 23:23:04 +0800

    m

--- impl/java/Test.java
+++ impl/java/Test.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Test {
     public static void main( String args[] ) {
+        System.out.println( perlin.noise( 0.1,0.1, 0.1 ) );
         Random rnd = new Random();
         int seed = rnd.nextInt();
         System.out.println( perlin.noise( 0.1,0.1, 10*seed ) );
